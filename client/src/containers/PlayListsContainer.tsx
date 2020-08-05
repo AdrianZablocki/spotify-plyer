694eea6ccf306bd2fff398b9075539fa5d5ad2ce
ADRIAN ZABLOCKI <adrian.zablocki@execon.pl>
2020-08-05 23:10:13 +0200

    New: Split to smaller components

--- client/src/containers/PlayListsContainer.tsx
+++ client/src/containers/PlayListsContainer.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import Carousel from 'nuka-carousel';
 
+import PlayListCarousel from 'src/components/PlayListCarousel';
 import usePlayLists from 'src/hooks/use-playlists';
 
 function PlayListsContainer(): JSX.Element {
@@ -12,12 +12,6 @@ function PlayListsContainer(): JSX.Element {
     history.push('/account');
   }, [history]);
 
-  const redirectToPlaylist = useCallback((id: string) => {
-    history.push(`/playlist/${id}`, {
-      id,
-    });
-  }, []);
-
   useEffect(() => {
     sendRequest();
   }, []);
@@ -27,16 +21,7 @@ function PlayListsContainer(): JSX.Element {
       {isLoading && <div>spiner</div>}
       {hasError && <div>error message</div>}
       <button onClick={redirectToAccount} type="button">go to account</button>
-      {response && (
-        <Carousel cellAlign="center" cellSpacing={20}>
-          {
-            response.items.map((item: any) =>
-              <div key={item.id} onClick={() => redirectToPlaylist(item.id)}>
-                <img src={item.images[0].url} style={{ height: 250, width: 250, borderRadius: '50%', margin: '0 auto', display: 'block' }} />
-              </div>)
-          }
-        </Carousel>
-      )}
+      {response && <PlayListCarousel playLists={response.items} />}
     </div>
   );
 }
