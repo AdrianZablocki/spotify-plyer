4526b0a2883ccfbb75d9a4fed90e30c6922ab21a
ADRIAN ZABLOCKI <adrian.zablocki@execon.pl>
2020-08-18 13:10:37 +0200

    New: Account container - styles

--- client/src/containers/AccountContainer.tsx
+++ client/src/containers/AccountContainer.tsx
@@ -8,10 +8,12 @@ import useUser from 'src/hooks/use-user';
 const AccountContainerWrapper = styled.div`
  display: flex;
  flex-direction: column;
- justify-content: center;
+ justify-content: flex-end;
  align-items: center;
  width: 100%;
  height: 100%;
+ padding-bottom: 50px;
+ box-sizing: border-box;
 `;
 
 const UserWrapper = styled.div`
@@ -19,6 +21,30 @@ const UserWrapper = styled.div`
  flex-direction: column;
  justify-content: center;
  align-items: center;
+`;
+
+const User = styled.div`
+  margin-bottom: 50px;
+  color: #FFF;
+  font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+  letter-spacing: .5px;
+  text-align: center;
+`;
+
+const UserName = styled.div`
+  font-size: 22px;
+  color: #0FD55A;
+  margin-bottom: 10px;
+`;
+const UserMail = styled.div`
+  font-size: 16px;
+  color: #626262;
+`;
+const UserAccountType = styled.div`
+  text-transform: uppercase;
+  margin-top: 10px;
+  font-size: 14px;
+  color: #626262;
 `;
 
 function AccountContainer(): JSX.Element {
@@ -39,9 +65,11 @@ function AccountContainer(): JSX.Element {
       {hasError && <div>error message</div>}
       {response && (
         <UserWrapper data-test="section-account2">
-          <div className="huj">User: {response.display_name}</div>
-          <div>Email: {response.email}</div>
-          <div>Type: {response.product}</div>
+          <User>
+            <UserName>{response.display_name}</UserName>
+            <UserMail>{response.email}</UserMail>
+            <UserAccountType>{response.product}</UserAccountType>
+          </User>
           <ButtonPrimary click={redirectToPlayLists} content="Go to play list" />
         </UserWrapper>
       )}
