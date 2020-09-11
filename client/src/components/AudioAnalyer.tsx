b2ab61a5ae1663cf2ee3e56d0a81b56e455865db
ADRIAN ZABLOCKI <adrian.zablocki@execon.pl>
2020-09-11 13:34:00 +0200

    Fix: Remove logs

--- client/src/components/AudioAnalyer.tsx
+++ client/src/components/AudioAnalyer.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import { Camera, AudioListener } from 'three';
-
-// const camera = new Camera();
 
 interface Properties {
   test?: number;
 }
 
 function AudioAnalyser({ test }: Properties): JSX.Element {
-  const camera = new Camera();
-  const listener = new AudioListener();
-  camera.add(listener);
-
-  console.log('analyser', camera);
   return (
     <div>analyzer</div>
   );
