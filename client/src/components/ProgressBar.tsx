9174d4bdad6e65588fa91be19959dd84fb2af811
ADRIAN ZABLOCKI <adrian.zablocki@execon.pl>
2020-09-11 10:39:50 +0200

    New: Progressbar - styles

--- client/src/components/ProgressBar.tsx
+++ client/src/components/ProgressBar.tsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled';
 import React from 'react';
 
 import durationConverter from 'src/utility/duration-converter';
@@ -7,11 +8,56 @@ interface Properties {
   timer: number;
 }
 
+const ProgressbarWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Time = styled.span`
+  font-size: 10px;
+  font-weight: 700;
+  color: #626262;
+  text-transform: uppercase;
+`;
+const BarWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 1px;
+  margin: 0 10px;
+  background: #000;
+  &:after {
+    content: '';
+    width: 5px;
+    height: 5px;
+    background: #000;
+    border-radius: 50%;
+    position: absolute;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+`;
+const Bar = styled.div`
+  position: absolute;
+  width: 30%;
+  height: 5px;
+  top: 50%;
+  left: 0;
+  background: #626262;
+  transform: translateY(-50%);
+`;
+
 function Progressbar({ duration, timer }: Properties): JSX.Element {
   const seconds = `${timer}`;
 
   return (
-    <div>0:{seconds.padStart(2, '0')} progressbar {durationConverter(duration)}</div>
+    <ProgressbarWrapper>
+      <Time>0:{seconds.padStart(2, '0')}</Time>
+      <BarWrapper>
+        <Bar />
+      </BarWrapper>
+      <Time>{durationConverter(duration)}</Time>
+    </ProgressbarWrapper>
   );
 }
 export default Progressbar;
